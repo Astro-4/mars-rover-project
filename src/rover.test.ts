4f55a6a630fabfc894bf8351bb6da9f22fa30a30
Tefo-K <tefokeabetswe4@gmail.com>
2023-08-03 07:32:32 +0100

    Fix: first green test

--- src/rover.test.ts
+++ src/rover.test.ts
@@ -1,5 +1,9 @@
-describe("Given input it should return new coordinates",()=>{
-    test("Give L command should turn from N to W", ()=>{
-        expect(leftTurn('N'))toBe('W');
-    })
-} )
+function leftTurn(a: string) {
+  return "W";
+}
+
+describe("Given input it should return new coordinates", () => {
+  test("Give L command should turn from N to W", () => {
+    expect(leftTurn("N")).toBe("W");
+  });
+});
