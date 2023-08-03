fe9230efb03e7bbf6197573f4615929a59f82ef3
Tefo-K <tefokeabetswe4@gmail.com>
2023-08-03 07:57:43 +0100

    Feat: leftTurn function in the green stage

--- src/rover.test.ts
+++ src/rover.test.ts
@@ -1,13 +1,21 @@
 const leftTurn = (a: string) => {
   if (a === "N") return "W";
-  return "S";
+  if (a === "W") return "S";
+  if (a === "S") return "E";
+  return "N";
 };
 
-describe("Given input it should return new coordinates", () => {
+describe("Given current direction it should turn to the correct direction", () => {
   test("should turn from N to W", () => {
     expect(leftTurn("N")).toBe("W");
   });
   test("should turn from W to S", () => {
     expect(leftTurn("W")).toBe("S");
+  });
+  test("should turn from S to E", () => {
+    expect(leftTurn("S")).toBe("E");
+  });
+  test("should turn from E to N", () => {
+    expect(leftTurn("E")).toBe("N");
   });
 });
