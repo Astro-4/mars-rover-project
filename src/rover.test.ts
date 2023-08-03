cf7ab68b337206c0fddfd731cd47933d3bf43fd5
Tefo-K <tefokeabetswe4@gmail.com>
2023-08-03 07:43:47 +0100

    Feat: turn from west to south

--- src/rover.test.ts
+++ src/rover.test.ts
@@ -1,7 +1,13 @@
-const leftTurn = (a: string) => "W";
+const leftTurn = (a: string) => {
+  if (a === "N") return "W";
+  return "S";
+};
 
 describe("Given input it should return new coordinates", () => {
-  test("Give L command should turn from N to W", () => {
+  test("should turn from N to W", () => {
     expect(leftTurn("N")).toBe("W");
+  });
+  test("should turn from W to S", () => {
+    expect(leftTurn("W")).toBe("S");
   });
 });
