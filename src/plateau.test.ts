1ba2ab8aca6cff1942c0086f594924a306316a4c
Tefo-K <tefokeabetswe4@gmail.com>
2023-08-16 19:02:10 +0100

    feat: implemented the plateau creation function and tests.

---
+++ src/plateau.test.ts
@@ -0,0 +1,16 @@
+import { createPlateau } from "./plateau";
+
+describe("Given current direction it return true for inputs between 0 and 5", () => {
+  test("should return the axis for inputs between 0 and 5", () => {
+    expect(createPlateau([0, 0])).toBe("0, 0");
+    expect(createPlateau([5, 5])).toBe("5, 5");
+    expect(createPlateau([1, 3])).toBe("1, 3");
+    expect(createPlateau([2, 4])).toBe("2, 4");
+  });
+  test("should return undefined for inputs outside boundaries", () => {
+    expect(createPlateau([-1, -1])).toBe(undefined);
+    expect(createPlateau([6, 6])).toBe(undefined);
+    expect(createPlateau([-1, 3])).toBe(undefined);
+    expect(createPlateau([5, 6])).toBe(undefined);
+  });
+});
